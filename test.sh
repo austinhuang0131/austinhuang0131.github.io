ee6162458a7f66477ffc87eefa558c88c88eeb1b
Austin Huang <im@austinhuang.me>
2021-12-10 10:15:18 -0500

    ugh

--- test.sh
+++ test.sh
@@ -10,24 +10,24 @@ for Line in $Lines
 do
     if [[ $Line =~ \[([0-9a-z]+\.[0-9a-z]+)\] ]]; then
         raw="${BASH_REMATCH[1]}"
-        base=$(curl -L "https://$raw/.well-known/matrix/server" | jq '."m.server"' | sed s/\"//g)
-        if [[ -z "$base" ]] || [ "$base" == "null" ]; then
-            DNS=$(curl "https://dns.google/resolve?name=_matrix._tcp.$raw&type=SRV" | jq '.Answer[0].data' | sed -E 's/(^"|"$|([0-9]+ ){2})//g')
+        base=$(curl -Ls -m 10 "https://$raw/.well-known/matrix/server" | jq '."m.server"' | sed s/\"//g)
+        if [[ -z "$base" ]] || [ base == "null" ]; then
+            echo "Looking up SRV of $raw"
+            DNS=$(dig "_matrix._tcp.$raw" SRV +short | sed -E 's/([0-9]+ ){2}//g')
+            echo "$DNS"
             SRV_PORT=$(echo "$DNS" | grep -oE "^\d+")
             SRV_DOMAIN=$(echo "$DNS" | grep -oE "([a-z0-9]+\.)+$" | sed 's/\.$//')
             if [[ -n "$SRV_PORT" ]] && [[ -n "$SRV_DOMAIN" ]]; then
-                body=$(curl -L "https://$SRV_DOMAIN:$SRV_PORT/_matrix/federation/v1/version")
+                body=$(curl -Ls -m 10 "https://$SRV_DOMAIN:$SRV_PORT/_matrix/federation/v1/version")
             else
-                body=$(curl -L "https://$raw/_matrix/federation/v1/version")
+                body=$(curl -Ls -m 10 "https://$raw/_matrix/federation/v1/version")
             fi
         else
-            body=$(curl -L "https://$base/_matrix/federation/v1/version")
+            body=$(curl -Ls -m 10 "https://$base/_matrix/federation/v1/version")
         fi
         name=$(echo $body | jq .server.name | sed s/\"//g)
         version=$(echo $body | jq .server.version | sed s/\"//g)
-        if [[ "$name" == "Synapse" ]] && [[ -n "$version" ]]; then
-            echo "$Line $version |" >> _includes/matrix_prod.md
-        elif [[ -n "$name" ]] && [[ -n "$version" ]]; then
+        if [[ -n "$name" ]] && [[ -n "$version" ]]; then
             echo "$Line $name $version |" >> _includes/matrix_prod.md
         else
             echo "$Line Error!! |" >> _includes/matrix_prod.md
